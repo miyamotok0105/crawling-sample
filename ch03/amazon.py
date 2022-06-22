63d58982194e5765d049239951a4905d93c91b25
KengoTobita <jp0003menegi@gmail.com>
2022-06-22 17:39:32 +0900

    add amazon and log func

--- ch03/amazon.py
+++ ch03/amazon.py
@@ -22,67 +22,88 @@ class Mercari_serch_option:
 
 class Mercari_item_info:
     id = None
+    url = None
     datetime = None
     title = None
     price = None
+    stock = None
     postage = None
     description = None
     image_urls = list()
 
+# class Mercari_scrape(Mercari_item_info, Mercari_serch_option):
+    # def __init__(self):
+    #     logger = getLogger(__name__)
+    #     formatter = '[%(levelname)s] %(asctime)s %(filename)s.%(funcName)s.l%(lineno)d: %(message)s'
+    #     logging.basicConfig(level=logging.DEBUG, format=formatter)
+
+    # def export_csv(item_infos, out_path):
+    #     info_vec_2d = 
+    #     columns = ["id", "title", "price", "postage", "description", ""]
+    #     df = pd.Dataframe(info_vec_2d)
+    #     df.to_csv(out_path)
     
 def update_page_num(driver, page_num):
-    page_option = f"&page_token=v1%3A{page_num}"
+    page_option = f"&page={page_num}"
     driver.get(target_url + page_option)
 
 def get_item_urls(driver):
-    a_tag_elements = driver.find_elements_by_tag_name("a")
+    item_elements = driver.find_elements(By.CSS_SELECTOR, ".a-section a-spacing-base".replace(" ", "."))
+    a_tag_elements = [i.find_element(By.TAG_NAME, "a") for i in item_elements]
     hrefs = [i.get_attribute("href") for i in a_tag_elements]
-    item_urls = [i for i in hrefs if "item" in i]
+    item_urls = list(set(hrefs))
     return item_urls
 
 def get_item_info(driver):
     result = Mercari_item_info()
 
     # id
-    result.id = driver.current_url.split("/")[-1]
+    result.id = driver.current_url.split("/")[-2]
+    result.url = driver.current_url
     # datetime
     now = datetime.datetime.now()
-    result.datetime = now.strftime('%y/%m/%d %H:%M:%S')    
+    result.datetime = now.strftime('%y/%m/%d %H:%M:%S')
     # title
-    title_element = driver.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > div.mer-spacing-b-12 > mer-heading')
+    title_element = driver.find_element(By.ID, 'title')
     result.title = title_element.text
     # price
-    price_element = driver.find_element(By.CSS_SELECTOR, "#item-info > section:nth-child(1) > section:nth-child(2) > div > mer-price")
+    price_element = driver.find_element(By.CLASS_NAME, "a-price-whole")
     result.price = price_element.text
+    # is stock
+    stock_element = driver.find_element(By.ID, "availability")
+    result.stock = "In Stock" in stock_element.text
     # description
-    description_element = driver.find_element(By.CSS_SELECTOR, "#item-info > section:nth-child(2) > mer-show-more > mer-text")
+    description_element = driver.find_element(By.ID, "feature-bullets")
     result.description = description_element.text
     # sip cost
-    postage_sections = driver.find_element(By.CSS_SELECTOR, "#item-info > section.layout__StyledSection-sc-1lyi7xi-7.kdAFPN > div > mer-display-row:nth-child(4) > span:nth-child(2)")
-    result.postage = postage_sections.text
+    try:
+        postage_sections = driver.find_element(By.ID, "deliveryBlockMessage")
+        result.postage = postage_sections.text
+    except:
+        result.postage = None
     # images 
-    images_section= driver.find_elements(By.CLASS_NAME, "sticky-inner-wrapper")[0]
-    image_tags = images_section.find_elements(By.TAG_NAME, "mer-item-thumbnail")
+    images_section= driver.find_element(By.ID, "imageBlock")
+    image_tags = images_section.find_elements(By.TAG_NAME, "img")
     srcs = list(set([i.get_attribute("src") for i in image_tags]))
-    result.image_urls.extend(srcs)
-    
+    result.image_urls.append(srcs)
     return result
 
     
 if __name__=="__main__":
     try:
         driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-        target_url = "https://jp.mercari.com/search?keyword=novation&t1_category_id=1328&status=on_sale&category_id=79"
+        # target_url = "https://www.amazon.co.jp/s?k=シンセサイザー"
+        target_url = "https://www.amazon.co.jp/s?k=シンセサイザー+novation"
         driver.get(target_url)   
-
+        driver.maximize_window()
 
         logger = getLogger(__name__)
-        formatter = '[%(levelname)s] %(asctime)s %(filename)s.%(funcName)s.l%(lineno)d: %(message)s'
+        formatter = '[%(levelname)s] %(asctime)s / %(filename)s.%(funcName)s.L%(lineno)d: %(message)s'
         logging.basicConfig(level=logging.INFO, format=formatter)
 
         logger.info("start scrape item urls.")
-        time.sleep(10)
-        page_num=0
+        time.sleep(5)
+        page_num=1
         item_urls = list()
         while True:
             time.sleep(5)
@@ -99,7 +120,10 @@ if __name__=="__main__":
         for i_url in tqdm(item_urls):
             driver.get(i_url)   
             time.sleep(5)
-            item_infos.append(get_item_info(driver))
+            try:
+                item_infos.append(get_item_info(driver))
+            except Exception as e:
+                logger.info(f"coudn't parse page.{e}(url:{i_url})")
 
     except Exception as e :
         logger.error(e)       
