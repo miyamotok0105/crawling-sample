0e60bdebba39dff1a24368b47e93509b2c9b3e90
KengoTobita <jp0003menegi@gmail.com>
2022-12-18 17:16:06 +0900

    ちょっとリファクタ

--- ch03/tabelog.py
+++ ch03/tabelog.py
@@ -17,9 +17,28 @@ def get_next(driver):
     pagenation_element = driver.find_elements(By.CLASS_NAME, "c-pagination__item")[-1]
     pagenation_element.find_element(By.TAG_NAME, "a").click()
 
-def check_last(driver):
-    pagenation_element = driver.find_element(By.CLASS_NAME, "list-pagenation")
-    return True if "次" in pagenation_element.text else False
+def get_pagenum(driver):
+    count_elements = driver.find_elements(By.CLASS_NAME, "c-page-count__num")
+    paging_num = int(count_elements[1].text)
+    total_num = int(count_elements[2].text)
+    return total_num // paging_num
+
+def get_store_url(driver):
+    store_elements = driver.find_elements(By.CSS_SELECTOR, ".list-rst__wrap.js-open-new-window")
+    store_elements = [i.find_element(By.TAG_NAME, "h3") for i in store_elements]
+    store_elements = [i.find_element(By.TAG_NAME, "a") for i in store_elements]
+    return [i.get_attribute("href") for i in store_elements]
+
+def get_store_info(driver, url):
+    map_url = url + "dtlmap/"
+    driver.get(map_url)
+    time.sleep(SLEEP_TIME)
+    table_elements = driver.find_elements(By.CSS_SELECTOR, ".c-table.c-table--form.rstinfo-table__table")
+    outer_table = [i.get_attribute("outerHTML") for i in table_elements]    
+    df = pd.read_html(outer_table[0])[0]
+    columns = df[0].tolist()
+    values = df[1].tolist()
+    return {k:v for k,v in zip(columns, values)}
 
 if __name__=="__main__":
     try:
@@ -27,18 +46,12 @@ if __name__=="__main__":
         base_url = "https://tabelog.com/tokyo/rstLst/?vs=1&sa=%E6%9D%B1%E4%BA%AC%E9%83%BD&sk=%25E5%2588%2580%25E5%2589%258A%25E9%25BA%25BA&lid=top_navi1&vac_net=&svd=20220822&svt=1900&svps=2&hfc=1&Cat=RC&LstCat=RC03&LstCatD=RC0304&LstCatSD=RC030402&cat_sk=%E5%88%80%E5%89%8A%E9%BA%BA"        
         driver.get(base_url)
         time.sleep(SLEEP_TIME)
-        # get store page url
-        count_elements = driver.find_elements(By.CLASS_NAME, "c-page-count__num")
-        paging_num = int(count_elements[1].text)
-        total_num = int(count_elements[2].text)
-        page_num  = total_num // paging_num
+
+        page_num = get_pagenum(driver)
 
         store_urls = list()
         for i in range(page_num):
-            store_elements = driver.find_elements(By.CSS_SELECTOR, ".list-rst__wrap.js-open-new-window")
-            store_elements = [i.find_element(By.TAG_NAME, "h3") for i in store_elements]
-            store_elements = [i.find_element(By.TAG_NAME, "a") for i in store_elements]
-            urls = [i.get_attribute("href") for i in store_elements]
+            urls = get_store_url(driver)
             store_urls.extend(urls)
             print(urls)
             get_next(driver)
@@ -46,17 +59,10 @@ if __name__=="__main__":
 
         results = list()
         for i_url in store_urls:
-            map_url = i_url + "dtlmap/"
-            driver.get(map_url)
-            table_elements = driver.find_elements(By.CSS_SELECTOR, ".c-table.c-table--form.rstinfo-table__table")
-            outer_table = [i.get_attribute("outerHTML") for i in table_elements]    
-            df = pd.read_html(outer_table[0])[0]
-            columns = df[0].tolist()
-            values = df[1].tolist()
-            results.append({k:v for k,v in zip(columns, values)})
-            time.sleep(SLEEP_TIME)
+            store_info = get_store_info(driver, i_url)
+            results.append(store_info)
     finally:
         driver.quit()
 
     df = pd.DataFrame(results)
-    df.to_csv(CSV_NAME)
+    df.to_csv(CSV_NAME, index=False)
